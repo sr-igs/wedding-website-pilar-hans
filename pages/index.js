26718ce266424c48fea5bd51a3ad0f01f253282c
Iggy <iclavelb@gmail.com>
2022-11-15 07:59:34 +1100

    Try fix flaticon

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import InitialTitle from '../components/InitialTitle';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
@@ -7,11 +6,6 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Ignacio and Catherine 2023</title>
-        <meta name="description" content="Ignacio and Catherine 2023" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main>
         <div>
           <InitialTitle />
