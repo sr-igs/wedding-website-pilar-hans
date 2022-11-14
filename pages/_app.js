f232a8cff080668d28ac6787608e95874d2333fa
Iggy <iclavelb@gmail.com>
2022-11-15 07:51:18 +1100

    Update head to app and change favicon

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,19 @@ import { ThemeProvider,createTheme } from '@mui/material/styles';
 import { customTheme } from '../themes/customTheme';
 import { appWithTranslation } from 'next-i18next';
 import Footer from '../components/ui/Footer';
+import Head from 'next/head';
 
 const theme = createTheme(customTheme)
 
 function MyApp({ Component, pageProps }) {
   return <ThemeProvider theme={theme}>
     <Component {...pageProps} />
-    <Footer />
+      <Head>
+        <title>Ignacio and Catherine 2023</title>
+        <meta name="description" content="Ignacio and Catherine 2023" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Footer />
     </ThemeProvider>
 }
 
