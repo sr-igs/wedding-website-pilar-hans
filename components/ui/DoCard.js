7e06c2aaa701685bd9fbd4ca7d9560ed953c5342
Iggy <iclavelb@gmail.com>
2022-11-25 11:26:12 +1100

    Change img component in DoCard for picture with source

--- components/ui/DoCard.js
+++ components/ui/DoCard.js
@@ -1,13 +1,15 @@
 import styles from "./DoCard.module.css";
 import { Card } from "@mui/material";
-import Link from "next/link";
 
 function DoCard(props){
     return(
         <Card variant="outlined" sx={{width:"20rem"}}>
             <div className={styles.mainDiv}>
                 <h3>{props.title}</h3>
-                <img src={props.image} style={{width:"18rem"}} alt={`${props.title} image`} />
+                <picture>
+                    <source srcSet={props.image} type="image/webp" />
+                    <img src={props.image} style={{width:"18rem"}} alt={`${props.title} image`} />
+                </picture>
                 <p className={styles.description}>{props.description}</p>
                 <a target="_blank" rel="noreferrer" href={props.link} className={styles.link}>Website</a>
             </div>
